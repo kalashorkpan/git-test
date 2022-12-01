982eb23fa2eabebffd0ab0eb8556daf65d698c4a
kalashorkpan <58248384+kalashorkpan@users.noreply.github.com>
2022-12-01 18:51:19 +0800

    pull commit

--- src/main/java/com/atguitu/git/GitTest.java
+++ src/main/java/com/atguitu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello github!44");
         System.out.println("hello github!44");
         System.out.println("push github22");
+        System.out.println("pull github22");
 
     }
 }
