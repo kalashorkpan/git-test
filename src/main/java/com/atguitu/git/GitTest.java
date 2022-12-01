9fb5a757c74c1d8e8bd1f3ef8507c023f506257d
Layne <kalashorkpan@163.com>
2022-12-01 18:33:59 +0800

    hot-fox commit1

--- src/main/java/com/atguitu/git/GitTest.java
+++ src/main/java/com/atguitu/git/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello github!");
+        System.out.println("hello github!23");
+        System.out.println("hello github!33");
 
     }
 }
